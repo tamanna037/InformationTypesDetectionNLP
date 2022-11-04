fa2fc17aeef0ee1d83bc10633d54b2f89e425065
Caelan Neumann <caelann2@gmail.com>
2022-11-03 22:29:38 -0600

    Update InfoTypesDetectionOss.py
    Lint code.
    -added newlines between functions
    -renamed totalClass to total_class

--- code/InfoTypesDetectionOss.py
+++ code/InfoTypesDetectionOss.py
@@ -5,16 +5,15 @@ import datasets as dt
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
 from datasets import load_dataset, Dataset
-from transformers import AutoTokenizer,Trainer,DataCollatorWithPadding,AutoModelForSequenceClassification, TrainingArguments
+from transformers import AutoTokenizer, Trainer, DataCollatorWithPadding, AutoModelForSequenceClassification, TrainingArguments
 from sklearn.metrics import classification_report
 import numpy as np
 
-
-TOTAL_CLASS=13
+TOTAL_CLASS = 13
 tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
 
 
-#the goal of this function is to remove duplicate texts from the dataset and removing extra spaces from a text data
+# the goal of this function is to remove duplicate texts from the dataset and removing extra spaces from a text data
 def preprocess_data(issue_df):
     # remove duplicate rows by Text Content
     issue_df = issue_df.drop_duplicates(subset=['Text Content'], keep='first')
@@ -26,7 +25,9 @@ def preprocess_data(issue_df):
     issue_df['Text Content'] = issue_df['Text Content'].str.strip()
     return issue_df
 
-# Call this function to get the dataset and get necessary information about the dataset: length of the dataset, number of items in per class, total number of classes
+
+# Call this function to get the dataset and get necessary information about the dataset: length of the dataset,
+# number of items in per class, total number of classes
 def getDatasetInfo():
     issue_df = pd.read_csv('../data/dataInfoTypes.csv')
 
@@ -48,7 +49,7 @@ def getDatasetInfo():
     # Renaming code to label for better understanding
     issue_df = issue_df.rename(columns={'Text Content': 'text', 'Code': 'label'})
 
-    #replacing label string with unique int value
+    # replacing label string with unique int value
     label_encoder = preprocessing.LabelEncoder()
     label_encoder.fit(issue_df['label'])
     issue_df['label'] = label_encoder.transform(issue_df['label'])
@@ -56,6 +57,7 @@ def getDatasetInfo():
     print('\n\n------Final Columns on dataset---------')
     print(set(issue_df.head()))
     return issue_df
+
 
 # 90% data are on training set and 10% on testset. 5% of the training data has been taken to the validation set.
 def splitIntoTrainTestValSet(issue_df):
@@ -68,35 +70,40 @@ def splitIntoTrainTestValSet(issue_df):
     test_dataset = Dataset.from_dict(test_df)
 
     dataset = dt.DatasetDict({"train": train_dataset, "val": val_dataset, "test": test_dataset})
-    return dataset,train_df, val_df,test_df
+    return dataset, train_df, val_df, test_df
+
 
 # Class weight has been measured to handle the imbalance among class distribution and later fed into the trainer
 def getClassWeight(train_df):
+    total_class = len(set(train_df['label']))
+    class_weight = [0 for i in range(total_class)]
 
-    totalClass= len(set(train_df['label']))
-    class_weight = [0 for i in range(totalClass)]
-
-    for i in range(totalClass):
-        count=len(train_df[train_df['label'] == i])
-        class_weight[i]= (1 / count) * (len(train_df) / totalClass)
+    for i in range(total_class):
+        count = len(train_df[train_df['label'] == i])
+        class_weight[i] = (1 / count) * (len(train_df) / total_class)
 
     return class_weight
 
+
 def preprocess_function(examples, text_column_name="text"):
     return tokenizer(examples[text_column_name], truncation=True)
+
 
 def get_data_collator():
     data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
     return data_collator
 
+
 # Bert tokenizer and model has been used
 def tokenize(dataset):
-    tokenized_dataset= dataset.map(preprocess_function, batched=True)
+    tokenized_dataset = dataset.map(preprocess_function, batched=True)
     return tokenized_dataset
+
 
 def getModel():
     model = AutoModelForSequenceClassification.from_pretrained("bert-base-uncased", num_labels=TOTAL_CLASS)
     return model
+
 
 def getTraining_args():
     training_args = TrainingArguments(
@@ -110,29 +117,29 @@ def getTraining_args():
 
     return training_args
 
-#this function take dataset as a input, calculate class weights, tokenize text data and then train the model based on the text data only.
+
+# this function take dataset as a input, calculate class weights, tokenize text data and then train the model based
+# on the text data only.
 def train_model():
-
-    #loading dataset file into dataframe
+    # loading dataset file into dataframe
     issue_df = getDatasetInfo()
-
 
     # splitting dataset into 3 non overlapping sets: train,test, validation
     dataset, train_df, val_df, test_df = splitIntoTrainTestValSet(issue_df)
 
-    #for balancing this imbalanced dataset, classweight has been determined
+    # for balancing this imbalanced dataset, classweight has been determined
     class_weight = getClassWeight(train_df)
 
-    #tokenized the texts using bert
+    # tokenized the texts using bert
     tokenized_dataset = tokenize(dataset)
 
     data_collator = get_data_collator()
     model = getModel()
 
-    #set some hyperparameters
-    training_args=getTraining_args()
+    # set some hyperparameters
+    training_args = getTraining_args()
 
-    #a custometrainer for incorporating class weight
+    # a custometrainer for incorporating class weight
     class CustomTrainer(Trainer):
 
         def compute_loss(self, model, inputs, return_outputs=False):
@@ -160,7 +167,6 @@ def train_model():
     )
 
     trainer.train()
-
 
 
 train_model()
