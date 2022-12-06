24b400b9d68ef39858f2878e76dd98b0e24bb027
Tamanna <tamannacsebuet@gmail.com>
2022-12-06 13:44:08 -0700

    updated liscensing information

--- code/InfoTypesDetectionOss.py
+++ code/InfoTypesDetectionOss.py
@@ -1,3 +1,6 @@
+#  Copyright 2022 Tamanna, Licensed under MIT. For more information , check LICENSE.txt
+
+
 import pandas as pd
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
